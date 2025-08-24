37e89d0f8ce056f1a22e82645621d1a4cbef5146
Sergey Lavrent <91432388+hiimsergey@users.noreply.github.com>
2025-08-24 14:06:35 +0200

    INCORRECT Made something compilable

--- doto.zig
+++ doto.zig
@@ -8,51 +8,71 @@ const CONFIG = @import("config.zig").CONFIG;
 const ENTRY_TIMES: [CONFIG.len]u16 = blk: {
 	var result: [CONFIG.len]u16 = undefined;
 	for (CONFIG, 0..) |entry, i|
-		result[i] = entry.@"1" * @divFloor(MAX_PERDAYS, entry.@"2");
+		result[i] = entry.@"1" * @divFloor(MAX_PERIOD, entry.@"2");
 	break :blk result;
 };
-const MAX_PERDAYS: @FieldType(Entry, "2") = blk: {
+const MAX_PERIOD: @FieldType(Entry, "2") = blk: {
 	var result = 0;
 	for (CONFIG) |entry| result = @max(result, entry.@"2");
 	break :blk result;
 };
 const MAX_TASKS: u8 = blk: {
-	var total = 0;
+	var total: f32 = 0;
 	for (ENTRY_TIMES) |time| total += time;
 	break :blk @intFromFloat(@ceil(
-		@as(f32, @floatFromInt(total)) /
-		@as(f32, @floatFromInt(MAX_PERDAYS))
+		total /
+		@as(f32, @floatFromInt(MAX_PERIOD))
 	));
 };
-const Buffer = [MAX_PERDAYS * MAX_TASKS]?[]const u8;
+const Buffer = [MAX_PERIOD][MAX_TASKS]?[]const u8;
+
+const FMT_BLACK = "\x1b[30m";
+const FMT_NORMAL = "\x1b[39m";
 
 pub fn main() void {
 	const out = comptime std.io.getStdOut().writer();
-	const err = comptime std.io.getStdErr().writer();
+	const buf: Buffer = comptime get_buffer();
 
-	const buf: Buffer = comptime construct(err);
-	_ = buf;
+	const days_since_epoch: u64 = @abs(@divFloor(std.time.timestamp(), std.time.s_per_day));
+	const todays_period_i = days_since_epoch % MAX_PERIOD;
 
-	_ = out;
+	out.print(
+		FMT_BLACK ++ "Day {d}/{d}\n" ++ FMT_NORMAL,
+		.{todays_period_i, MAX_PERIOD}
+	) catch {};
+	for (buf[todays_period_i]) |name| {
+		if (name == null) break;
+		out.print("{s}\n", .{name.?}) catch {};
+	}
 }
 
-fn construct(err: anytype) Buffer {
-	const result = .{ null } ** @typeInfo(Buffer).array.len;
-
-	var cur_slot: u16 = 0;
+fn get_buffer() Buffer {
+	var result: Buffer = .{ .{ null } ** MAX_TASKS } ** MAX_PERIOD;
 	for (CONFIG, 0..) |entry, i| {
 		const total_times = ENTRY_TIMES[i];
-		const step = @divFloor(MAX_PERDAYS, total_times);
+		const step = @divFloor(MAX_PERIOD, total_times);
 
-		for (0..total_times) |time| {
-			_ = time;
-		}
-		_ = step;
-		_ = entry;
+		inline for (0..total_times) |time| put_entry(&result, entry.@"0", time * step);
 	}
-
-	cur_slot += 1;
-	_ = err;
-
 	return result;
+}
+
+inline fn put_entry(buf: *Buffer, name: []const u8, target_day: u16) void {
+	for (0..MAX_TASKS) |task| {
+		if (buf[target_day][task] == null) continue;
+		buf[target_day][task] = name;
+		return;
+	}
+	
+	var min_day: u16 = target_day;
+	var free_task: u16 = MAX_TASKS - 1;
+	for (0..target_day) |day| {
+		for (0..MAX_TASKS) |task| {
+			if (buf[day][task] != null or task >= free_task) continue;
+			min_day = day;
+			free_task = task;
+			break;
+		}
+	}
+	buf[min_day][free_task] = name;
 }
